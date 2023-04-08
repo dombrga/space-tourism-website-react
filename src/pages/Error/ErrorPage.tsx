4f5e8d7f35aae39d770431e5613f42ee2dab0294
modb <dominic.o.barga@accenture.com>
2023-04-08 17:02:58 +0800

    added error page

--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -1,11 +1,12 @@
 import { useRouteError } from "react-router-dom";
+import style from "./ErrorPage.module.scss";
 
 function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div id={style['error-page']}>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
