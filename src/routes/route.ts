494b4393f4b7e1715759ba3a13e501f6f905eb27
modb <dominic.o.barga@accenture.com>
2023-06-11 23:00:43 +0800

    merged testing branch to main

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -3,34 +3,41 @@ import { createBrowserRouter } from "react-router-dom";
 import RootPage from "./Root/RootPage";
 
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      Component: RootPage,
+      ErrorBoundary: ErrorPage,
+      children: [
+        {
+          path: '/home',
+          Component: HomePage,
+          id: 'home'
+        },
+        {
+          path: '/destination',
+          Component: DestinationPage,
+          id: 'destination'
+        },
+        {
+          path: '/crew',
+          Component: CrewPage,
+          id: 'crew'
+        },
+        {
+          path: '/technology',
+          Component: TechnologyPage,
+          id: 'technology'
+        },
+      ]
+    },
+  ],
   {
-    path: '/',
-    Component: RootPage,
-    ErrorBoundary: ErrorPage,
-    children: [
-      {
-        path: '/home',
-        Component: HomePage,
-        id: 'home'
-      },
-      {
-        path: '/destination',
-        Component: DestinationPage,
-        id: 'destination'
-      },
-      {
-        path: '/crew',
-        Component: CrewPage,
-        id: 'crew'
-      },
-      {
-        path: '/technology',
-        Component: TechnologyPage,
-        id: 'technology'
-      },
-    ]
-  },
-])
+    // basename: import.meta.env.MODE === 'production' ? import.meta.env.VITE_BASE_URL : undefined
+    // basename: '/space-tourism-website-react/'
+    // basename: 'https://dombrga.github.io/space-tourism-website-react/'
+  }
+)
 
 export default router
