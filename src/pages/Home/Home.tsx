d4dbe80798fa507684ffc4913cb3880f86186ab3
modb <dominic.o.barga@accenture.com>
2023-06-19 01:31:15 +0800

    initial home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,23 @@ import './Home.module.scss'
 import s from './Home.module.scss'
 
 const homePageClass = [s['home-page']].join(' ')
+const exploreClass = [s['explore']].join(' ')
 
 function HomePage() {
   return (
     <div className={homePageClass}>
-      home
+      <div>
+        <h5>So, you want to travel to</h5>
+        <h1>Space</h1>
+        <p className='body-text'>
+          Let's face it; if you want to go to space, you might as well genuinely go to outer space and not 
+          hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this
+          world experience!
+        </p>
+      </div>
+      <div className={exploreClass}>
+        Explore
+      </div>
     </div>
   );
 }
--- src/pages/Root/RootPage.tsx
+++ src/pages/Root/RootPage.tsx
@@ -2,16 +2,16 @@ import { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import BodyContent from '../../components/body-content/BodyContent'
 import TopHeader from '../../components/top-header/TopHeader';
+import s from './RootPage.module.scss'
+
+const mainClass = [s['main']].join(' ')
 
 function RootPage() {
-  
-  
   // console.log('location', location);
-  
   return (
     <BodyContent>
       <TopHeader />
-      <main>
+      <main className={mainClass}>
         <Outlet />
       </main>
     </BodyContent>
